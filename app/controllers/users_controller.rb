64e4d7ccba003dd787353028e9b61437d422d1ac
Rachid Rakaa <rachid1982fsb@gmail.com>
2019-12-28 12:13:05 -0800

    add serializer gem and generate serializers for the 3 models and tested them

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,21 @@
 class UsersController < ApplicationController
+
+    def index
+        users = User.all
+        render :json => users
+    end
+
+    def show
+        render :json => User.find(params[:id])
+    end
+
     def create
         user = User.new(user_params)
         if user.save
             # puts "great "
         end
     end 
-
-
+    
 private
 
     def  user_params
